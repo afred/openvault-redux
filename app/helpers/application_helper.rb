bbbe4b0bc1b0a7257bde0d0fe761e2801e4a70f9
Chris Beer <chris_beer@wgbh.org>
2011-02-22 13:09:46 -0500

    rendering helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 require 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def render_document_metadata_partial(document=@document, options={})
+    render :partial => 'document_metadata', :locals => { :document => document }
+  end
+
+  def render_document_heading(document=@document, options={})
+    render :partial => 'document_heading', :locals => { :document => document, :heading => document_heading }
+  end
 end
