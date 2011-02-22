5b4a024c1d1680a86cb9fa4302353e2585d0c699
Chris Beer <chris_beer@wgbh.org>
2011-02-22 13:09:26 -0500

    pull in blacklight routes from plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,35 @@
 ActionController::Routing::Routes.draw do |map|
-  Blacklight::Routes.build map
+  # Login, Logout, UserSessions
+  map.resources :user_sessions, :protocol => ((defined?(SSL_ENABLED) and SSL_ENABLED) ? 'https' : 'http')
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
+
+  # Set the default controller:
+  map.root :controller => 'catalog', :action=>'index'
+  map.resources :bookmarks, :collection => {:clear => :delete}
+  map.resource :user
+
+  map.catalog_facet "catalog/facet/:id", :controller=>'catalog', :action=>'facet'
+
+  map.resources :search_history, :collection => {:clear => :delete}
+  map.resources :saved_searches, :collection => {:clear => :delete}, :member => {:save => :put}
+
+  map.resources(:catalog,
+    :only => [:index, :show, :update],
+    # /catalog/:id/image <- for ajax cover requests
+    # /catalog/:id/status
+    # /catalog/:id/availability
+    :member=>{:image=>:get, :status=>:get, :availability=>:get, :librarian_view=>:get},
+    # /catalog/map
+    :collection => {:map => :get, :opensearch=>:get, :citation=>:get, :email=>:get, :sms=>:get, :endnote=>:get, :send_email_record=>:post},
+      :requirements => { :id => /([A-Za-z0-9]|:|-|\.)+/ }
+  )
+    
+
+  map.feedback 'feedback', :controller=>'feedback', :action=>'show'
+  map.feedback_complete 'feedback/complete', :controller=>'feedback', :action=>'complete'
+    
+  map.resources :folder, :only => [:index, :update, :destroy], :collection => {:clear => :delete }
 
   # The priority is based upon order of creation: first created -> highest priority.
 
