2d3d18d8cf5e9d13e3212f9a050ba145c87d0e7d
Chris Beer <chris@cbeer.info>
2011-02-24 14:10:52 -0500

    roles_users table

--- db/migrate/20110224190056_create_roles.rb
+++ db/migrate/20110224190056_create_roles.rb
@@ -7,9 +7,15 @@ class CreateRoles < ActiveRecord::Migration
 
       t.timestamps
     end
+    create_table "roles_users", :id => false, :force => true do |t|
+      t.references  :user
+      t.references  :role
+      t.timestamps
+    end
   end
 
   def self.down
     drop_table :roles
+    drop_table :roles_users
   end
 end
