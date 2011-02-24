f5a55e0bfaeda704f3dc65eace920fdb6b9438c6
Chris Beer <chris@cbeer.info>
2011-02-24 14:02:58 -0500

    add acl9

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  include Blacklight::User::UserGeneratedContent
+  include Blacklight::User::Authlogic
+
+  acts_as_authorization_subject :association_name => :roles
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,12 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "compass", :version => ">= 0.10.6"
   config.gem "nokogiri"
+  config.gem "streamly"
+  config.gem "sqlite3"
+  config.gem "fastercsv"
+  config.gem "rest-client", :lib => "rest_client"
+  config.gem "acl9"
+  gem "soap4r"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
