9654a7e2e4467640551b46904e3f318092aa486b
Chris Beer <chris@cbeer.info>
2011-02-25 10:29:53 -0500

    override blacklight document link to de-js it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,9 @@ module ApplicationHelper
 
   def render_document_heading(document=@document, options={})
     render :partial => 'document_heading', :locals => { :document => document, :heading => document_heading }
+   end
+  def link_to_document(doc, opts={:label=>Blacklight.config[:index][:show_link].to_sym, :counter => nil, :results_view => true})
+    label = render_document_index_label doc, opts
+    return link_to(label, catalog_path(doc[:id]))
   end
 end
