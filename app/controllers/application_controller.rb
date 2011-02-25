de65733fb0629ede7d1f07e05afac6f0cbc471d0
Chris Beer <chris@cbeer.info>
2011-02-25 15:12:12 -0500

    remove unused css/js includes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,9 @@ class ApplicationController < ActionController::Base
     stylesheet_links << ["compiled/screen", { :media => 'screen, projection' }]
     stylesheet_links << ["compiled/print", { :media => 'print' }]
     stylesheet_links << ['jquery/ui-lightness/jquery-ui-1.8.1.custom.css',  {:plugin=>:blacklight, :media=>'all'}]
-    stylesheet_links << ["application", "openvault"]
 
     javascript_includes << ['jquery-1.4.2.min.js', 'jquery-ui-1.8.1.custom.min.js',  'application', { :plugin=>:blacklight } ]
-    javascript_includes << ['jquery.domsearch.js', 'liquidmetal.js', "application", "openvault"]
+    javascript_includes << ['jquery.domsearch.js', 'liquidmetal.js', "application"]
 
   end
 
