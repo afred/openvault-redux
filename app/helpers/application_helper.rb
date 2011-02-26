e824d2b7b766606ae9d7919f48c690928a673875
Chris Beer <chris@cbeer.info>
2011-02-26 16:11:01 -0500

    s/require/require_dependency/

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-require 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
+require_dependency 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def render_document_metadata_partial(document=@document, options={})
