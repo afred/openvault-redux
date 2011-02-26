6ade09f878d244a82930a519da6cd2a019bfc693
Chris Beer <chris@cbeer.info>
2011-02-26 16:11:48 -0500

    add gems for blacklight plugins

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -29,6 +29,9 @@ Rails::Initializer.run do |config|
   config.gem "fastercsv"
   config.gem "rest-client", :lib => "rest_client"
   config.gem "acl9"
+  config.gem "cql-ruby", :lib => "cql_ruby"
+  config.gem "rubytree", :lib => "tree", :version => '0.5.2'
+  config.gem "oai"
   require 'open-uri'
   gem "soap4r"
 
