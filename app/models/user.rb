db72d97b23f76f65546aa1cbeb557f7d50d56991
Chris Beer <chris_beer@wgbh.org>
2011-03-02 16:41:32 -0500

    rm user model

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  include Blacklight::User::UserGeneratedContent
-  include Blacklight::User::Authlogic
-
-  acts_as_authorization_subject :association_name => :roles
-
-end
