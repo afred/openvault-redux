eba07baa1c7adfe0867ac0dc99cf349da33f5477
Chris Beer <chris_beer@wgbh.org>
2011-03-03 12:16:55 -0500

    embed iframes

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -29,7 +29,19 @@ class CatalogController < ApplicationController
   end
 
   def embed
-    render 
+    @response, @document = get_solr_response_for_doc_id    
+    respond_to do |format|
+      format.html {render :layout => 'embed' }
+      
+      # Add all dynamically added (such as by document extensions)
+      # export formats.
+      @document.export_formats.each_key do | format_name |
+        # It's important that the argument to send be a symbol;
+        # if it's a string, it makes Rails unhappy for unclear reasons. 
+        format.send(format_name.to_sym) { render :text => @document.export_as(format_name) }
+      end
+      
+    end
   end
 
   # when a request for /catalog/BAD_SOLR_ID is made, this method is executed...
