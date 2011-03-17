2f420585a7c9ba22a9a01ea45c1584635edd10de
Chris Beer <chris@cbeer.info>
2011-03-17 11:34:53 -0400

    add placeholder media url mapping

--- lib/openvault/media.rb
+++ lib/openvault/media.rb
@@ -1,5 +1,17 @@
 module Openvault::Media
   def self.filename_to_url filename
+    case filename
+      when /\.mp4$/
+        "http://openvault.wgbh.org/media/videos/#{filename}"
+      when /\.mp3$/
+        "http://openvault.wgbh.org/media/audio/#{filename}"
+      when /\.flv$/
+        "http://openvault.wgbh.org/media/audio/#{filename}"
+      when /\.jpg$/
+        "http://openvault.wgbh.org/media/thumbnails/#{filename}"
+      when /\.xml$/
+        "http://openvault.wgbh.org/media/transcripts/#{filename}"
+    end
 
   end
 end
