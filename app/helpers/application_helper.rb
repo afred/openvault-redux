154978a6506bd6bbd3bf90848a2a2eb25ec0f415
Chris Beer <chris_beer@wgbh.org>
2011-03-29 10:52:03 -0400

    move document partials around in preparation for new show view design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,10 @@ require_dependency 'vendor/plugins/blacklight/app/helpers/application_helper.rb'
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def render_document_partial(doc, action_name)
-    format = document_partial_name(doc)
+    format = self.send "document_#{action_name}_partial_name", doc if self.respond_to? "document_#{action_name}_partial_name"
+    format ||= document_partial_name(doc)
     begin
-      enforce_rights(doc, action_name)
+      enforce_rights(doc, action_name) if action_name == "media"
       render :partial=>"catalog/_#{action_name}_partials/#{format}", :locals=>{:document=>doc}
     rescue Openvault::PermissionDenied
       render :partial=>"catalog/_#{action_name}_partials/permission_denied", :locals=>{:document=>doc}
@@ -20,13 +21,10 @@ module ApplicationHelper
     end
   end
 
-  def render_document_metadata_partial(document=@document, options={})
-    render :partial => 'document_metadata', :locals => { :document => document }
-  end
-
   def render_document_heading(document=@document, options={})
     render :partial => 'document_heading', :locals => { :document => document, :heading => document_heading }
-   end
+  end
+
   def link_to_document(doc, opts={:label=>Blacklight.config[:index][:show_link].to_sym, :counter => nil, :results_view => true})
     label = render_document_index_label doc, opts
     return link_to(label, catalog_path(doc[:id]))
