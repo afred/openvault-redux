36ced363f6cdab99a84b0bd1442aee4ea6435e11
Chris Beer <chris_beer@wgbh.org>
2011-04-19 13:50:09 -0400

    update from blacklight solr search params logic update

--- lib/openvault/solr_helper/facet_domsearch.rb
+++ lib/openvault/solr_helper/facet_domsearch.rb
@@ -1,6 +1,6 @@
 module Openvault::SolrHelper::FacetDomsearch
-  def solr_facet_params(facet_field, extra_controller_params={})
-    solr_params = super(facet_field, extra_controller_params={})
+  def solr_facet_params(facet_field, user_params = params || {}, extra_controller_params={})
+    solr_params = super(facet_field, user_params, extra_controller_params)
     solr_params[:"f.#{facet_field}.facet.limit"] = 1000
     solr_params
   end
