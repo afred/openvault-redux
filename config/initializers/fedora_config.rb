7b89033cd27e4b40d4d54558b25811db5e14b597
Chris Beer <chris_beer@wgbh.org>
2011-04-21 12:50:25 -0400

    load only the soap api endpoint

--- config/initializers/fedora_config.rb
+++ config/initializers/fedora_config.rb
@@ -2,8 +2,7 @@ config = YAML::load(File.open("#{RAILS_ROOT}/config/fedora.yml"))
 
 Rubydora.repository = Rubydora.connect({:url => config[RAILS_ENV]['url'], :user => config[RAILS_ENV]['user'], :password => config[RAILS_ENV]['pass']})
 
-require 'rubydora/rest_api_client/v33'
-Rubydora.repository.extend Rubydora::RestApiClient::V33     
+Rubydora.repository.extend Rubydora::Soap
 
 require 'rubydora/ext/solr'
 Rubydora::Ext::Solr.load
