68f0bbf4bedd5227ac13ca3b2a025289ed7a1b1a
Chris Beer <chris_beer@wgbh.org>
2011-05-08 16:31:18 +0000

    fix problem with malformed pbcore

--- lib/openvault/datastreams/pbcore.rb
+++ lib/openvault/datastreams/pbcore.rb
@@ -3,8 +3,6 @@ module Openvault::Datastreams
      def to_solr solr_doc = {}
       super(solr_doc)
 
-      begin
-
       solr_doc['pbcore_s'] = content
       pbcore = Nokogiri::XML(content) 
       xmlns = { 'pbcore' => 'http://www.pbcore.org/PBCore/PBCoreNamespace.html'}
@@ -12,22 +10,22 @@ module Openvault::Datastreams
       pbcore.xpath('/pbcore:PBCoreDescriptionDocument/pbcore:*', xmlns).each do |tag|
         case tag.name
         when 'pbcoreIdentifier'
-          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:identifierSource/text()', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:identifier/text()', xmlns).first.text]
+          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:identifierSource', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:identifier', xmlns).first.text]
         when 'pbcoreTitle'
-          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:titleType/text()', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:title/text()', xmlns).first.text]
+          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:titleType', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:title', xmlns).first.text]
         when 'pbcoreSubject'
-          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:subjectAuthorityUsed/text()', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:subject/text()', xmlns).first.text]
+          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:subjectAuthorityUsed', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:subject', xmlns).first.text]
 
         when 'pbcoreDescription'
-          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:descriptionType/text()', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:description/text()', xmlns).first.text]
+          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:descriptionType', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:description', xmlns).first.text]
         when 'pbcoreContributor'
-          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:contributorRole/text()', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:contributor/text()', xmlns).first.text]
+          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:contributorRole', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:contributor', xmlns).first.text]
         when 'pbcorePublisher'
-          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:publisherRole/text()', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:publisher/text()', xmlns).first.text]
+          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:publisherRole', xmlns).first.text.parameterize.to_s rescue nil}_s",  tag.xpath('pbcore:publisher', xmlns).first.text] if tag.xpath('pbcore:publisher', xmlns).first
         when 'pbcoreCreator'
-          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:creatorRole/text()', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:creator/text()', xmlns).first.text]
+          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:creatorRole', xmlns).first.text.parameterize.to_s rescue nil}_s",  tag.xpath('pbcore:creator', xmlns).first.text] if tag.xpath('pbcore:creator', xmlns).first
         when 'pbcoreCoverage'
-          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:coverageType/text()', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:coverage/text()', xmlns).first.text]
+          doc << ["pbcore_#{tag.name}_#{tag.xpath('pbcore:coverageType', xmlns).first.text.parameterize.to_s}_s",  tag.xpath('pbcore:coverage', xmlns).first.text]
       end
 
 
@@ -40,7 +38,7 @@ module Openvault::Datastreams
             case tag.name
               when 'pbcoreAnnotation'
               when 'pbcoreFormatID'
-                doc << ["pbcore_pbcoreInstantiation_pbcoreFormatID_#{tag.xpath('pbcore:formatIdentifierSource/text()', xmlns).first.text.parameterize.to_s}_s", tag.xpath('pbcore:formatIdentifier/text()', xmlns).first.text]
+                doc << ["pbcore_pbcoreInstantiation_pbcoreFormatID_#{tag.xpath('pbcore:formatIdentifierSource', xmlns).first.text.parameterize.to_s}_s", tag.xpath('pbcore:formatIdentifier', xmlns).first.text]
               else
                 doc << ["pbcore_pbcoreInstantiation_#{tag.name}_s", tag.text]
             end
@@ -52,9 +50,6 @@ module Openvault::Datastreams
           solr_doc[key] ||= []
           solr_doc[key] <<  value.strip
         end
-
-      rescue
-      end
      end 
    end
 end
