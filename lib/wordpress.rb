4237ad24ca9946e393decb4eac93d59b9b64d34b
Chris Beer <chris_beer@wgbh.org>
2011-05-10 15:57:48 -0400

    wordpress api refinement

--- lib/wordpress.rb
+++ lib/wordpress.rb
@@ -1,42 +1,4 @@
 module Wordpress
-
-  def self.endpoint
-    @endpoint
-  end
-
-  def self.endpoint= endpoint
-    @endpoint = endpoint
-  end
-
-  class Post
-    def self.find id
-      parsed_json = ActiveSupport::JSON.decode(open("#{endpoint}/#{id}?json=1").read)
-      raise "Missing Wordpress Post" unless parsed_json["status"] == "ok"
-      Post.new(parsed_json)
-    end
-
-    attr_accessor :page
-
-    def initialize json
-      page = parsed_json["page"]
-    end
-
-    def title
-      page["title"]
-    end
-
-    def custom_fields
-      page["custom_fields"]
-    end
-
-    def thumbnail
-      page["thumbnail"]
-    end
-
-    def content
-      page["content"]
-    end
-  end
-
-  class Page < Post; end
+  autoload :Post, 'wordpress/post'
+  autoload :Page, 'wordpress/page'
 end
