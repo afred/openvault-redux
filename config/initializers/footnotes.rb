8675d10db8fda39828cdf85716d61fe17cb8f325
Chris Beer <chris_beer@wgbh.org>
2011-06-02 09:58:05 -0400

    remove cookies footnotes pane

--- config/initializers/footnotes.rb
+++ config/initializers/footnotes.rb
@@ -1,4 +1,5 @@
-if defined?(Footnotes) && Rails.env.development?
+if defined?(Footnotes) && Rails.env.development? #&& defined?(Rails::Server)
+  Footnotes::Filter.notes -= [:cookies]
   Footnotes.run!
    Footnotes::Filter.prefix = 'mvim://open?url=file://%s&amp;line=%d&amp;column=%d'
 end
