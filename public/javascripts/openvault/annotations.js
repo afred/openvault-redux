1d8f9092b10c8d7a18570e6f04fdf596323a103e
Chris Beer <chris_beer@wgbh.org>
2011-06-13 11:49:42 -0400

    Fix undefined check

--- public/javascripts/openvault/annotations.js
+++ public/javascripts/openvault/annotations.js
@@ -11,7 +11,7 @@
          dialogContainer: '#annotations_modal_dialog',
          load: function(event, dialog) {
            md = $('.record_annotation_metadata', dialog).append($('.media_fragment_template .media_fragment').clone());
-           if(player != null) {
+           if(typeof(player) != "undefined") {
              try {
              $('#comment_metadata_begin', md).val(s_to_timestamp(parseInt(player.getPosition())));
              $('#comment_metadata_end', md).val(s_to_timestamp(parseInt(player.getPosition())));
