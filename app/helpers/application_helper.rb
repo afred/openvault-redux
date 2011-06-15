d7e4d15475d73d35321cfcdfb67de3e650a90f45
Chris Beer <chris_beer@wgbh.org>
2011-06-15 17:50:45 -0400

    add arg.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -91,7 +91,7 @@ module ApplicationHelper
     nil
   end
 
-  def render_field_value
+  def render_field_value value=nil
     value = [value] unless value.is_a? Array
     value = value.collect { |x| x.respond_to?(:force_encoding) ? x.force_encoding("UTF-8") : x}
     return value.join(field_value_separator).html_safe
