c5f529f6cb63e234f6b84cb18bdb5091dd27becb
Chris Beer <chris_beer@wgbh.org>
2011-07-06 14:16:58 -0400

    need devise encryptable for authlogic_sha hashing

--- db/migrate/20090101000000_devise_create_users.rb
+++ db/migrate/20090101000000_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.rememberable
       t.trackable
 
-      # t.encryptable
+      t.encryptable
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
