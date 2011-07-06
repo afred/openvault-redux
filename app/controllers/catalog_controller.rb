9fd64c4b327bef9c314f24c6db457418efa77577
Chris Beer <chris_beer@wgbh.org>
2011-07-06 13:33:32 -0400

    change redirects to 301 permanent redirects

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -133,17 +133,17 @@ class CatalogController < ApplicationController
   # when a request for /catalog/BAD_SOLR_ID is made, this method is executed...
   def invalid_solr_id_error
     response, documents = get_solr_response_for_field_values("pid_s",params[:id])
-    redirect_to url_for(:id => documents.first.id) and return if documents.length > 0
+    redirect_to url_for(:id => documents.first.id), :status=>301 and return if documents.length > 0
 
     response, documents = get_solr_response_for_field_values("pid_short_s",params[:id])
-    redirect_to url_for(:id => documents.first.id) and return if documents.length > 0
+    redirect_to url_for(:id => documents.first.id), :status=>301 and return if documents.length > 0
 
     if RAILS_ENV == "development"
       render # will give us the stack trace
     else
     #  flash[:notice] = "Sorry, you have requested a record that doesn't exist."
     #  redirect_to root_path, :status => 404
-      render
+      render(:file => "#{RAILS_ROOT}/public/404.html", :layout => false, :status => 404)
     end
     
   end
