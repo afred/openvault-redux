1ce3c0c024976cbffb8ff594f53de3afcac590df
Chris Beer <chris_beer@wgbh.org>
2011-07-06 13:33:47 -0400

    Force collections controller to show every result

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,8 @@
 class CollectionsController < CatalogController
   include Openvault::SolrHelper::Collections
 
+  def solr_search_params(*args)
+    super(*args).merge :per_page => 5000
+  end
+
 end
