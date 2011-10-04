b34f4e32341ee70695a82cf9a722090c2bb6408a
Chris Beer <chris_beer@wgbh.org>
2011-10-04 13:51:40 -0400

    Explicitly dup the solr search params logic array

--- app/controllers/user_generated_content_controller.rb
+++ app/controllers/user_generated_content_controller.rb
@@ -1,4 +1,5 @@
 class UserGeneratedContentController < CatalogController
+  self.solr_search_params_logic = self.solr_search_params_logic.dup
   include BlacklightUserGeneratedContent::SolrHelper::Scope
   module Helper
     def document_partial_name *args
