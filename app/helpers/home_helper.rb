e3d44db7e948372d7c7e41c01f5b373611c95b30
Chris Beer <chris_beer@wgbh.org>
2011-10-14 10:18:29 -0400

    don't use asset pipeline version of mosaic img

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,7 +2,7 @@ module HomeHelper
   def image_sprite sprite, document
     offset = sprite[document]
     unless extra_head_content.any? { |x| x =~ /sprite-#{sprite.name}/ }
-      extra_head_content << "<style type='text/css'> .sprite-#{sprite.name} { height: #{sprite.options[:height]}px; width: #{sprite.options[:width]}px; display: inline-block; background-image: url(#{image_path("sprites/#{sprite.name}.jpg")}); background-repeat: no-repeat; text-indent: -9999px; position: relative; } </style>"
+      extra_head_content << "<style type='text/css'> .sprite-#{sprite.name} { height: #{sprite.options[:height]}px; width: #{sprite.options[:width]}px; display: inline-block; background-image: url(/images/sprites/#{sprite.name}.jpg); background-repeat: no-repeat; text-indent: -9999px; position: relative; } </style>"
     end
     content_tag :span, document.get(:title_display), :class => "sprite-#{sprite.name}", :style =>"background-position: -#{offset[:x]}px -#{offset[:y]}px; ", :title => document.get(:title_display) 
   end
